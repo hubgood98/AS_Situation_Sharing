7cca63f2d004d9e9c749f3eeaf97166d1011254d
hubgood98 <heej1998@gmail.com>
2024-08-14 14:21:01 +0900

    fix : 시큐리티 설정 추가

---
+++ src/main/java/com/example/as_situation_sharing_web/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.as_situation_sharing_web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+
+        return http.build();
+
+    }
+}
