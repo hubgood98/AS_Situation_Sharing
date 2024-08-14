f6379794796f4535bbb874993770a19663d2c11f
hubgood98 <heej1998@gmail.com>
2024-08-15 00:08:18 +0900

    임시커밋

--- src/test/java/com/example/as_situation_sharing_web/AsSituationSharingWebApplicationTests.java
+++ src/test/java/com/example/as_situation_sharing_web/AsSituationSharingWebApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.as_situation_sharing_web;
 
+import com.example.as_situation_sharing_web.service.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AsSituationSharingWebApplicationTests {
+
+    @Autowired
+    private QuestionService questionService;
 
     @Test
     void contextLoads() {
